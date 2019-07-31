ef97c28fce1fea40586d25de4297a4da28e4bbad
NieneB <niene.boeijen@gmail.com>
2019-07-31 10:33:06 +0200

    clean up

--- components/CustomMap.js
+++ components/CustomMap.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import MapGL, { NavigationControl } from 'react-map-gl';
-
+import MapGL from 'react-map-gl';
 
 const navStyle = {
   position: 'absolute',
@@ -21,22 +20,19 @@ class CustomMap extends React.Component {
         bearing: 0,
         pitch: 50,
         width: 900,
-        height: 800
+        height: 900
       }
     };
   }
 
   render() {
     const { viewport } = this.state;
-
     return (
       <MapGL
-        {...viewport}
         {...this.state.viewport}
         onViewportChange={(viewport) => this.setState({ viewport })}
         mapStyle={this.props.mapStyle}
       >
-
       </MapGL>
     );
   }
